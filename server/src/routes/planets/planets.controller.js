45e7ae0e6b887dd94fe2abc0b4d148c95d85c2b7
dpawson905 <dpawson905@gmail.com>
2023-01-14 13:59:14 -0500

    work on databases

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -2,6 +2,6 @@ const {
   getAllPlanets
 } = require('../../models/planets.model');
 
-exports.httpGetAllPlanets = (req, res, next) => {
-  return res.status(200).json(getAllPlanets());
+exports.httpGetAllPlanets = async (req, res, next) => {
+  return res.status(200).json(await getAllPlanets());
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
+require("dotenv").config();
+const cluster = require("cluster");
+cluster.schedulingPolicy = cluster.SCHED_RR;
 const PORT = process.env.PORT || 8000;
+const { MONGO_URL } = process.env;
 const app = require("./app");
 
 const http = require("http");
+const mongoose = require("mongoose");
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+ 
+mongoose.connection.on("error", (err) => {
+  console.error("We have error: " + err);
+});
+
 async function startServer() {
+  mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`NASA LC API is on port ${PORT}`);
